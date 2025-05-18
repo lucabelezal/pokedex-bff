1b29ef167ee3f110e987e86b65f1541122215ba7
Lucas Nascimento <17522772+lucabelezal@users.noreply.github.com>
2025-05-18 16:06:45 -0300

    first commit

---
+++ src/main/kotlin/com/pokedex/bff/PokedexBffApplication.kt
@@ -0,0 +1,11 @@
+package com.pokedex.bff
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PokedexBffApplication
+
+fun main(args: Array<String>) {
+	runApplication<PokedexBffApplication>(*args)
+}
