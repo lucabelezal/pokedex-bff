5bf061c7c986af97f737f509c0c6f50823da43fa
Lucas Nascimento <17522772+lucabelezal@users.noreply.github.com>
2025-05-18 21:22:37 -0300

    corrige import do csv para o banco

--- src/main/kotlin/com/pokedex/bff/controllers/CsvUploadController.kt
+++ src/main/kotlin/com/pokedex/bff/controllers/CsvUploadController.kt
@@ -1,6 +1,7 @@
 package com.pokedex.bff.controllers
 
-import com.pokedex.bff.services.CsvImportService
+import com.pokedex.bff.service.CsvImportService
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
+import java.io.IOException
 
 @RestController
 @RequestMapping("/csv")
@@ -16,8 +18,17 @@ class CsvUploadController(
 ) {
     @PostMapping("/import")
     fun uploadCsv(@RequestParam("file") file: MultipartFile): ResponseEntity<String> {
-        csvImportService.importPokemonCsv(file)
-        return ResponseEntity.ok("CSV importado com sucesso")
+        if (file.isEmpty) {
+            return ResponseEntity.badRequest().body("O arquivo está vazio.")
+        }
+        try {
+            csvImportService.importPokemonFromCsv(file.inputStream)
+            return ResponseEntity.ok("CSV importado com sucesso.")
+        } catch (e: IOException) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao ler o arquivo: ${e.message}")
+        } catch (e: Exception) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro durante a importação do CSV: ${e.message}")
+        }
     }
 
     @GetMapping("/test")
