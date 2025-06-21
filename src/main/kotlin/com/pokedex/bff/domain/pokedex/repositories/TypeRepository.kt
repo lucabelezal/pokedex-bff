79d85c4267cce99717b57354bd9f877f91ef0bdc
Lucas Nascimento <17522772+lucabelezal@users.noreply.github.com>
2025-06-21 15:26:40 -0300

    refactor: remova código não utilizado

--- src/main/kotlin/com/pokedex/bff/domain/pokedex/repositories/TypeRepository.kt
+++ src/main/kotlin/com/pokedex/bff/domain/pokedex/repositories/TypeRepository.kt
@@ -3,6 +3,4 @@ package com.pokedex.bff.domain.pokedex.repositories
 import com.pokedex.bff.infrastructure.persistence.entities.TypeEntity
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface TypeRepository : JpaRepository<TypeEntity, Long> {
-    fun findByNameIn(names: List<String>): List<TypeEntity>
-}
+interface TypeRepository : JpaRepository<TypeEntity, Long>
