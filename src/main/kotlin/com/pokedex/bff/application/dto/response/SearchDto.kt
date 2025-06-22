7b3b86caa7feb268cc3b3113fcfc72227d9f336f
Lucas Nascimento <17522772+lucabelezal@users.noreply.github.com>
2025-06-22 12:00:29 -0300

    doc: adiciona novas informações para swagger

--- src/main/kotlin/com/pokedex/bff/application/dto/response/SearchDto.kt
+++ src/main/kotlin/com/pokedex/bff/application/dto/response/SearchDto.kt
@@ -1,5 +1,12 @@
 package com.pokedex.bff.application.dto.response
 
+import io.swagger.v3.oas.annotations.media.Schema
+
+@Schema(description = "Informações sobre a busca atual na Pokedex")
 data class SearchDto(
+    @Schema(
+        description = "Texto de placeholder para campo de busca",
+        example = "Search Pokemons by name or number"
+    )
     val placeholder: String
 )
